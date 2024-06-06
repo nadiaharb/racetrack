718306b9e79400a2bc5c02cbafa3ef5a60a834a3
taavitaht <taavi@DESKTOP-BHBADCM.localdomain>
2024-06-06 15:33:55 +0300

    flags update when mode is changed

--- public/js/race-flags.js
+++ public/js/race-flags.js
@@ -1,8 +1,36 @@
 const socket = io();
 
-
-
 // Update flag display
 socket.on('raceModeChange', mode => {    
-    document.getElementById('modeDisplay').textContent = mode
+    
+    let color1
+    let color2
+
+    if (mode == "Safe") {
+        color1 = 'green'
+        color2 = 'green'
+    }
+    if (mode == "Hazard") {
+        color1 = 'yellow'
+        color2 = 'yellow'
+    }
+    if (mode == "Danger") {
+        color1 = 'red'
+        color2 = 'red'
+    }
+    if (mode == "Finish") {
+        color1 = 'white'
+        color2 = 'black'
+    }
+
+    let whiteSquares = document.getElementsByClassName("white");
+    for (let i = 0; i < whiteSquares.length; i++) {
+        whiteSquares[i].style.backgroundColor = color1;
+    }
+    let blackSquares = document.getElementsByClassName("black");
+    for (let i = 0; i < blackSquares.length; i++) {
+        blackSquares[i].style.backgroundColor = color2;
+    }
+
+    console.log("Updated flag display to: " + mode)
 })
