3ee777e75e6a2e87bc6fcef0dd483e58eb0ee926
taavitaht <taavi@DESKTOP-BHBADCM.localdomain>
2024-06-07 11:08:39 +0300

    finish confirmation

--- public/js/race-control.js
+++ public/js/race-control.js
@@ -3,20 +3,28 @@ const socket = io();
 // Add liteners to buttons
 // Safe button
 document.getElementById('safeBtn').addEventListener('click', () => {
-    console.log("clicked")
+    console.log("Clicked on Safe")
     socket.emit('raceModeChange', 'Safe')
 })
 // Hazard button
 document.getElementById('hazardBtn').addEventListener('click', () => {
+    console.log("Clicked on Hazard")
     socket.emit('raceModeChange', 'Hazard')
 })
 // Danger button
 document.getElementById('dangerBtn').addEventListener('click', () => {
+    console.log("Clicked on Danger")
     socket.emit('raceModeChange', 'Danger')
 })
 // Finish button
 document.getElementById('finishBtn').addEventListener('click', () => {
-    socket.emit('raceModeChange', 'Finish')
+    let text = "Confirm finishing the race";
+    if (confirm(text) == true) {
+        console.log("Race is finished by safety official")    
+        socket.emit('raceModeChange', 'Finish')  
+    } else {
+        console.log("Finish cancelled")
+    }    
 })
 
 // Update mode display in safety official interface
